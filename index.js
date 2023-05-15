d324556e2e5fe2dfc45aff4a338e3a7518bc0512
Akanksha Kumari Sinha <akrsinha202@gmail.com>
2023-05-16 00:50:08 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+//var randomDiceImage="dice"+ randomNumber1 +".png";
+var randomImage1Source="images/dice" + randomNumber1 +".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImage1Source);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomImage2Source="images/dice" + randomNumber2 +".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2Source);
+
+
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw!";
+}
